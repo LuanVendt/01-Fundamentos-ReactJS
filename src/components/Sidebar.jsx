3f3e56c6695288ddc738dd3f253e9d5fff38794d
Luan Vendt <luanvendt@gmail.com>
2023-11-24 16:04:46 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +10,15 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/111096073?v=4" alt="" />
+
                 <strong>Luan Vendt</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu Perfil
                 </a>
             </footer>

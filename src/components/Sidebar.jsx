d996edb4f80134ee4eb73d8360e0b2a290ebe4c7
Luan Vendt <luanvendt@gmail.com>
2023-11-27 15:14:32 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -10,7 +11,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/111096073?v=4" alt="" />
+                <Avatar src="https://github.com/LuanVendt.png"/>
 
                 <strong>Luan Vendt</strong>
                 <span>Web Developer</span>

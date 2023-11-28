ee9e2e334aa24ae014c9e782bf4482b40094b93c
Luan Vendt <luanvendt@gmail.com>
2023-11-28 13:56:58 -0300

    Inserindo comentários (Programação declarativa)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,9 +9,10 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
-        1,
-        2,
+        'Post muito bacana, hein?'
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -22,10 +23,17 @@ export function Post({ author, publishedAt, content }) {
         addSuffix: true,
     })
 
-    function handleCreateNewComment() {
+    function handleCreateNewComment(event) {
         event.preventDefault()
 
-        setComments([...comments, comments.length++]);
+        const newCommentText = event.target.querySelector('textarea').value
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('')
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value)
     }
 
     return (
@@ -58,7 +66,10 @@ export function Post({ author, publishedAt, content }) {
                 <strong>Deixe seu feedback</strong>
 
                 <textarea 
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -68,7 +79,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment} />
                 })}
             </div>
         </article>

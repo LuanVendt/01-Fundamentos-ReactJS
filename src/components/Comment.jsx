087a23dc285c84447287ad3c37b8e6cbd0d0c298
Luan Vendt <luanvendt@gmail.com>
2023-11-28 14:44:23 -0300

    Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment(props) {
+export function Comment({ content, onDeleteComment}) {
+    function handleDeleteComment() {
+        
+        onDeleteComment(content)
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/111096073?v=4" alt="" />
@@ -15,12 +20,12 @@ export function Comment(props) {
                             <time title='24 de Novembro às 16:18h' dateTime='20220-05-11 16:17:30'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24}/>
                         </button>
                     </header>
 
-                    <p>{props.content}</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
